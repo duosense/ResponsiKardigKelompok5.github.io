161b36b1e3b18f49b746588a2a69b390fcde2a92
Anggi <91518863+duosense@users.noreply.github.com>
2023-11-21 20:39:43 +0700

    Add files via upload

---
+++ data/TempatwisatadiKotaBogor_1.js
@@ -0,0 +1,15 @@
+var json_TempatwisatadiKotaBogor_1 = {
+"type": "FeatureCollection",
+"name": "TempatwisatadiKotaBogor_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": "1", "Nama tempa": "Kebun Raya Bogor", "x": "106.799558", "y": -6.597739, "web": "https:\/\/id.wikipedia.org\/wiki\/Kebun_Raya_Bogor", "gambar": "Tempat wisata bogor\/resize\/kebun raya.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.799558, -6.597739 ] } },
+{ "type": "Feature", "properties": { "No": "2", "Nama tempa": "The Jungle Water Adventure", "x": "106.795236", "y": -6.634558, "web": "https:\/\/www.thejungleadventure.com\/\n", "gambar": "Tempat wisata bogor\/resize\/the jungle.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.795236, -6.634558 ] } },
+{ "type": "Feature", "properties": { "No": "3", "Nama tempa": "Devoyage Bogor", "x": "106.795356", "y": -6.637125, "web": "https:\/\/devoyagebogor.net\/\n", "gambar": "Tempat wisata bogor\/resize\/Devoyage-Bogor.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.795356, -6.637125 ] } },
+{ "type": "Feature", "properties": { "No": "4", "Nama tempa": "Marcopolo Water Adventure", "x": "106.782683", "y": -6.547525, "web": "https:\/\/www.nativeindonesia.com\/marcopolo-bogor\/", "gambar": "Tempat wisata bogor\/resize\/marcopolo.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.791165, -6.59573 ] } },
+{ "type": "Feature", "properties": { "No": "5", "Nama tempa": "Museum of Zoology", "x": "106.796925", "y": -6.603705, "web": "https:\/\/en.wikipedia.org\/wiki\/Bogor_Zoology_Museum\n", "gambar": "Tempat wisata bogor\/resize\/Zoology.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.796925, -6.603705 ] } },
+{ "type": "Feature", "properties": { "No": "6", "Nama tempa": "Tirtania Waterpark", "x": "106.805438", "y": -6.640881, "web": "https:\/\/travelspromo.com\/htm-wisata\/tirtania-waterpark-bogor\/#google_vignette\n", "gambar": "Tempat wisata bogor\/resize\/Tirtania.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.805438, -6.640881 ] } },
+{ "type": "Feature", "properties": { "No": "7", "Nama tempa": "Indonesia Museum of Ethnobotany", "x": "106.793964", "y": -6.599121, "web": "https:\/\/id.wikipedia.org\/wiki\/Museum_Nasional_Sejarah_Alam_Indonesia\n", "gambar": "Tempat wisata bogor\/resize\/Museum-Nasional-Sejarah-Alam-Indonesia.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.793964, -6.599121 ] } },
+{ "type": "Feature", "properties": { "No": "8", "Nama tempa": "Jbound", "x": "106.797383", "y": -6.618234, "web": "https:\/\/jbound.net\/", "gambar": "Tempat wisata bogor\/resize\/Jbound.jpg" }, "geometry": { "type": "Point", "coordinates": [ 106.797383, -6.618234 ] } }
+]
+}
